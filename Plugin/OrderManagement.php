f2704c98f583a2df5b478f638f3377a9fc89eb4d
lidanhazout <lidan.hazout@transmitsecurity.com>
2023-07-10 17:55:33 +0300

    License System xml fix

--- Plugin/OrderManagement.php
+++ Plugin/OrderManagement.php
@@ -6,6 +6,7 @@ use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderManagementInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Encryption\EncryptorInterface;
+use Psr\Log\LoggerInterface
 
 class OrderManagement
 {
@@ -19,9 +20,11 @@ class OrderManagement
     public function __construct(
         ScopeConfigInterface $scopeConfig,
         EncryptorInterface $encryptor,
+        LoggerInterface $logger
     ) {
         $this->scopeConfig = $scopeConfig;
         $this->encryptor = $encryptor;
+        $this->logger = $logger;
     }
     /**
      * @param OrderManagementInterface $subject
@@ -34,18 +37,19 @@ class OrderManagement
         OrderManagementInterface $subject,
         OrderInterface $order
     ): array {
-        echo 'Handling beforePlace DRS plugin';
+        $this->logger->info('Handling beforePlace DRS plugin');
         $enableDeny = $this->scopeConfig->getValue('security_extension_section/security_extension_group/enable_deny');
-        echo $enableDeny;
-        $actionToken = $order->getData("actionToken");
-        echo $actionToken;
+        $this->logger->info($enableDeny);
+        $actionToken = $order->getData('actionToken');
+        $this->logger->info($actionToken);
+
         if ($actionToken == null || $enableDeny == null || $enableDeny == false) {
             return [$order];
         }
-        $response = $this->getRecommendation($actionToken);
-        $this->getRecommendation($actionToken);
-        if ($response['recommendation']['type'] == 'DENY') {
-            echo 'Deny recommendation';
+        $recommendation = $this->getRecommendation($actionToken);
+
+        if ($recommendation == 'DENY') {
+            $this->logger->info('Deny recommendation');
             return [];
        }
         return [$order];
@@ -65,9 +69,10 @@ class OrderManagement
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $accessToken]);
         $response = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
+            $this->logger->error('Error: ' . curl_error($ch));
         } else {
             $recommendation = $response['recommendation']['type'];
+            $this->logger->info('recommendation: ' . $recommendation);
         }
         curl_close($ch);
         return $recommendation;
@@ -94,7 +99,7 @@ class OrderManagement
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
         $response = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
+            $this->logger->error('Error: ' . curl_error($ch));
             return null;
         }
         curl_close($ch);
